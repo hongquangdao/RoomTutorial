b973a9cbc30b43f117473bb9a2d2ac231d5a5446
DHQ_930111 <daoquang1101@gmail.com>
2021-06-29 19:19:30 +0700

    Start RoomTutorial

---
+++ app/src/main/java/com/home/roomtutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.home.roomtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
